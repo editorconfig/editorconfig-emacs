b7befd3e88bf257e0b46b280104922e41fd5b9db
Desmond O. Chang <dochang@gmail.com>
2014-09-27 23:19:56 -0700

    edconf-set-indentation sets the indent and use local variable instead of global.
    Squashed commit of the following:
    
    commit ec1b84ac68ab9309a8ece62ec9fb0f6155b8207c
    Author: Desmond O. Chang <dochang@gmail.com>
    Date:   Tue Mar 4 23:14:23 2014 +0800
    
        Allow derived mode to use parent mode setting.
    
        Allow derived mode to use the setting of the closest parent mode which
        exists in `edconf-indentation-alist`.
    
        E.g., `doctex-mode` doesn't have to add its own setting into the alist.
        Instead, it can use the setting of `latex-mode` if they use the same
        indent variables.
    
    commit 8d0339d0cd2fe287074548a740cda3318c67d2e8
    Author: Desmond O. Chang <dochang@gmail.com>
    Date:   Tue Mar 4 19:27:34 2014 +0800
    
        More indentation settings.
    
    commit 46d0fc6cfa6b388714626258addbf9e547516cae
    Author: Desmond O. Chang <dochang@gmail.com>
    Date:   Fri Dec 13 05:23:31 2013 +0800
    
        Set indentation by mode.
    
    commit 5cf94a13185aadda80077feecfac4019fb5f25e2
    Author: Desmond O. Chang <dochang@gmail.com>
    Date:   Fri Dec 13 03:40:56 2013 +0800
    
        Make all related variables buffer local.
    
    commit df3423d111819b01f3ba20ddac47e389c48d0856
    Author: Desmond O. Chang <dochang@gmail.com>
    Date:   Fri Dec 13 03:52:48 2013 +0800
    
        Fix position of docstring.

--- editorconfig.el
+++ editorconfig.el
@@ -46,47 +46,134 @@
 
 (defvar edconf-exec-path "editorconfig")
 
+(defcustom edconf-indentation-alist
+  '((emacs-lisp-mode lisp-indent-offset)
+    (lisp-mode lisp-indent-offset)
+    (c-mode c-basic-offset)
+    (c++-mode c-basic-offset)
+    (objc-mode c-basic-offset)
+    (java-mode c-basic-offset)
+    (idl-mode c-basic-offset)
+    (pike-mode c-basic-offset)
+    (awk-mode c-basic-offset)
+    (cmake-mode cmake-tab-width)
+    (coffee-mode coffee-tab-width)
+    (cperl-mode cperl-indent-level)
+    (css-mode css-indent-offset)
+    (haskell-mode haskell-indent-spaces
+                  haskell-indent-offset
+                  shm-indent-spaces)
+    (js-mode js-indent-level)
+    (json-mode js-indent-level)
+    (js2-mode js2-basic-offset)
+    (js3-mode js3-indent-level)
+    (perl-mode perl-indent-level)
+    (python-mode . edconf-set-indentation/python-mode)
+    (ruby-mode ruby-indent-level)
+    (sh-mode sh-basic-offset sh-indentation)
+    (nxml-mode nxml-child-indent (nxml-attribute-indent . 2))
+    (sgml-mode sgml-basic-offset)
+    (livescript-mode livescript-tab-width)
+    (mustache-mode mustache-basic-offset)
+    (scala-mode scala-indent:step)
+    (groovy-mode c-basic-offset)
+    (latex-mode . edconf-set-indentation/latex-mode)
+    (web-mode (web-mode-indent-style . (lambda (size) 2))
+              web-mode-markup-indent-offset
+              web-mode-css-indent-offset
+              web-mode-code-indent-offset))
+  "Alist of indentation setting methods by modes.
+
+Each element looks like (MODE . FUNCTION) or (MODE . INDENT-SPEC-LIST).
+
+If FUNCTION is provided, it will be called when setting the indentation.  The
+indent size will be passed.
+
+If INDENT-SPEC-LIST is provided, each element of it must have one of the
+following forms:
+
+ 1. VARIABLE
+
+    It means (VARIABLE . 1).
+
+ 2. (VARIABLE . SPEC)
+
+    Setting VARIABLE according to the type of SPEC:
+
+      - Integer
+
+        The value is (* SPEC INDENT-SIZE);
+
+      - Function
+
+        The value is (funcall SPEC INDENT-SIZE);
+
+      - Any other type.
+
+        The value is SPEC.
+
+NOTE: Only the **buffer local** value of VARIABLE will be set."
+  :type '(alist :key-type symbol :value-type sexp)
+  :risky t
+  :group 'editorconfig)
+
+(defun edconf-set-indentation/python-mode (size)
+  (set (make-local-variable (if (or (> emacs-major-version 24)
+                                    (and (= emacs-major-version 24)
+                                         (>= emacs-minor-version 3)))
+                                'python-indent-offset
+                              'python-indent))
+       size)
+  ;; For https://launchpad.net/python-mode
+  (when (boundp 'py-indent-offset)
+    (set (make-local-variable 'py-indent-offset) size)))
+
+(defun edconf-set-indentation/latex-mode (size)
+  (set (make-local-variable 'tex-indent-basic) size)
+  (set (make-local-variable 'tex-indent-item) size)
+  (set (make-local-variable 'tex-indent-arg) (* 2 size))
+  ;; For AUCTeX
+  (when (boundp 'TeX-brace-indent-level)
+    (set (make-local-variable 'TeX-brace-indent-level) size))
+  (when (boundp 'LaTeX-indent-level)
+    (set (make-local-variable 'LaTeX-indent-level) size))
+  (when (boundp 'LaTeX-item-indent)
+    (set (make-local-variable 'LaTeX-item-indent) (- size))))
+
 (defun edconf-set-indentation (style &optional size tab_width)
-  (setq web-mode-indent-style 2)
   "Set indentation type from given style and size"
-  (when (equal style "space")
-    (setq indent-tabs-mode nil)
-    (if size
-      (setq size (string-to-number size)
-            LaTeX-indent-level size
-            LaTeX-item-indent size
-            TeX-brace-indent-level size
-            c-basic-offset size
-            cmake-tab-width size
-            coffee-tab-width size
-            cperl-indent-level size
-            css-indent-offset size
-            haskell-indent-offset size
-            shm-indent-spaces size
-            js-indent-level size
-            js2-basic-offset size
-            js3-indent-level size
-            lisp-indent-offset size
-            perl-indent-level size
-            py-indent-offset size
-            python-indent size
-            ruby-indent-level size
-            sh-basic-offset size
-            sh-indentation size
-            web-mode-markup-indent-offset size
-            web-mode-css-indent-offset size
-            web-mode-code-indent-offset size
-            ;(make-local-variable 'sgml-basic-offset) size
-            tab-stop-list (let ((stops (cons size ())))
-                            (while (< (car stops) 120)
-                              (setq stops (cons
-                                           (+ size (car stops))
-                                           stops)))
-                            (nreverse stops)))))
-  (when (equal style "tab")
-    (setq indent-tabs-mode t))
-  (if tab_width
-      (setq tab-width (string-to-number tab_width))))
+  (make-local-variable 'indent-tabs-mode)
+  (make-local-variable 'tab-width)
+  (when (and size (not (equal size "tab")))
+    (setq size (string-to-number size)))
+  (setq tab-width (cond (tab_width (string-to-number tab_width))
+                        ((numberp size) size)
+                        (t tab-width)))
+  (when (equal size "tab")
+    (setq size tab-width))
+  (cond ((equal style "space")
+         (setq indent-tabs-mode nil))
+        ((equal style "tab")
+         (setq indent-tabs-mode t)))
+  (when size
+    (let ((parent major-mode)
+          entry)
+      ;; Find the closet parent mode of `major-mode' in
+      ;; `edconf-indentation-alist'.
+      (while (and (not (setq entry (assoc parent edconf-indentation-alist)))
+                  (setq parent (get parent 'derived-mode-parent))))
+      (when entry
+        (let ((fn-or-list (cdr entry)))
+          (cond ((functionp fn-or-list) (funcall fn-or-list size))
+                ((listp fn-or-list)
+                 (dolist (elem fn-or-list)
+                   (cond ((symbolp elem) (set (make-local-variable elem) size))
+                         ((consp elem)
+                          (let ((spec (cdr elem)))
+                            (set (make-local-variable (car elem))
+                                 (cond ((functionp spec) (funcall spec size))
+                                       ((integerp spec) (* spec size))
+                                       (t spec))))))))))))))
 
 (defun edconf-set-line-ending (end-of-line)
   "Set line ending style to CR, LF, or CRLF"
@@ -102,13 +189,13 @@
   (cond
    ((equal final-newline "true")
     ;; keep prefs around how/when the nl is added, if set - otherwise add on save
-    (setq      require-final-newline      (or require-final-newline t)
-               mode-require-final-newline (or mode-require-final-newline t)))
+    (set      (make-local-variable 'require-final-newline)      (or require-final-newline t))
+    (set      (make-local-variable 'mode-require-final-newline) (or mode-require-final-newline t)))
    ((equal final-newline "false")
     ;; FIXME: Add functionality for actually REMOVING any trailing newlines here!
     ;;        (rather than just making sure we don't automagically ADD a new one)
-    (setq      require-final-newline nil
-               mode-require-final-newline nil))))
+    (set      (make-local-variable 'require-final-newline) nil)
+    (set      (make-local-variable 'mode-require-final-newline) nil))))
 
 (defun edconf-set-trailing-ws (trim-trailing-ws)
   "set up trimming of trailing whitespace at end of lines"
