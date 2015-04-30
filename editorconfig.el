73eae9a7cbdb030f7c85b155b27f784773bfb29d
jupl <juan.placencia.512@gmail.com>
2015-04-30 09:02:54 -0500

    Syntax highlighting for .editorconfig files

--- editorconfig.el
+++ editorconfig.el
@@ -291,6 +291,8 @@ NOTE: Only the **buffer local** value of VARIABLE will be set."
 ;;;###autoload
 (add-hook 'find-file-hook 'edconf-find-file-hook)
 
+(add-to-list 'auto-mode-alist '("\\.editorconfig" . conf-unix-mode)
+
 (provide 'editorconfig)
 
 ;;; editorconfig.el ends here
