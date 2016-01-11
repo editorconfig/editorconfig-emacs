73bcd85ec4b9fa53dfe1798a322051eb25577686
10sr <8slashes+git@gmail.com>
2016-01-11 23:14:45 +0900

    Check package metadata using ert framework

---
+++ ert-tests/metadata.el
@@ -0,0 +1,12 @@
+(require 'package)
+
+(defvar metadata-el-files nil)
+
+(ert-deftest test-metadata ()
+  (dolist (el metadata-el-files)
+    (message "Loading info: %s"
+      el)
+    (with-temp-buffer
+      (insert-file-contents el)
+      (message "%S"
+        (package-buffer-info)))))
