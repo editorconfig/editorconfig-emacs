765797d42784f15d207b5a4bfddce780eae2fbb3
10sr <8slashes+git@gmail.com>
2016-01-17 14:40:03 +0900

    Bump version: 0.7.0

--- editorconfig.el
+++ editorconfig.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011-2015 EditorConfig Team
 
 ;; Author: EditorConfig Team <editorconfig@googlegroups.com>
-;; Version: 0.6.2
+;; Version: 0.7.0
 ;; URL: https://github.com/editorconfig/editorconfig-emacs#readme
 ;; Package-Requires: ((editorconfig-core "0.6.2"))
 
