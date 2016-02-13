95defc516eb26285e6c023cdc134b1d97ba185bc
10sr <8slashes+git@gmail.com>
2016-02-14 02:23:39 +0900

    Add a test case for plugin test

--- ert-tests/editorconfig.el
+++ ert-tests/editorconfig.el
@@ -12,3 +12,20 @@
 (ert-deftest has-feature-01 nil
   "minimally working - provides 'editorconfig"
   (should (featurep 'editorconfig)))
+
+(ert-deftest test-apply-when-find-file ()
+  "Test if editorconfig-apply is called when visiting file."
+  (editorconfig-mode 1)
+  (prefer-coding-system 'utf-8-unix)
+  (let ((files-dir (concat default-directory
+                     "ert-tests/editorconfig-plugin-tests/test_files/")))
+    (dolist (target-charset '(
+                               ("latin1.txt" . iso-latin-1-unix)
+                               ("utf-8.txt" . utf-8-unix)))
+      (let ((target (concat files-dir
+                      (car target-charset))))
+        (with-current-buffer (find-file-noselect target)
+          (should (eq buffer-file-coding-system
+                    (cdr target-charset))))))
+
+  ))
