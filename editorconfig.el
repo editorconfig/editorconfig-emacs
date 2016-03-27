1201680112e79959d9f84b5782f049b2de363396
Hong Xu <hong@topbug.net>
2016-03-26 17:04:43 -0700

    Bump required editorconfi-core version
    Fix #80

--- editorconfig.el
+++ editorconfig.el
@@ -5,7 +5,7 @@
 ;; Author: EditorConfig Team <editorconfig@googlegroups.com>
 ;; Version: 0.7.3
 ;; URL: https://github.com/editorconfig/editorconfig-emacs#readme
-;; Package-Requires: ((editorconfig-core "0.6.2"))
+;; Package-Requires: ((editorconfig-core "0.7.3"))
 
 ;; See
 ;; https://github.com/editorconfig/editorconfig-emacs/graphs/contributors
