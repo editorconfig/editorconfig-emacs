1d4acc3ec73609990eabacb59c038708bc401003
Nick <opiation@users.noreply.github.com>
2022-02-28 22:32:56 -0500

    Constrain documentation strings to 80 chars (#269)
    * fix: wrap `editorconfig--legacy-version` docstring at 80 chars
    
    This small change addresses the annoying (comp) warning about this
    variable's documentation string exceeding the 80-character recommended
    limit.
    
    * fix: escape open parens at column 0 in doc string
    
    [`emacs-lisp-checkdoc` recommends][0] escaping open parentheses in strings
    when they begin at column 0. This escape the open parenthesis in the
    `editorconfig-hack-properties-functions` documentation string.
    
    [0]: https://github.com/emacs-mirror/emacs/blob/master/lisp/emacs-lisp/checkdoc.el#L1521

--- editorconfig.el
+++ editorconfig.el
@@ -148,7 +148,7 @@ This hook will be run even when there are no matching sections in
 These functions will be run after loading \".editorconfig\" files and before
 applying them to current buffer, so that you can alter some properties from
 \".editorconfig\" before they take effect.
-(Since 2021/08/30 (v0.9.0): Buffer coding-systems are set before running
+\(Since 2021/08/30 (v0.9.0): Buffer coding-systems are set before running
 this functions, so this variable cannot be used to change coding-systems.)
 
 For example, Makefiles always use tab characters for indentation: you can
@@ -852,8 +852,8 @@ F is that function, and FILENAME and ARGS are arguments passed to F."
 (defvar editorconfig--legacy-version nil
   "Use legacy version of editorconfig-mode.
 
-As of 2021/08/30, `editorconfig-mode' uses totally new implementation by default.
-This flag disables this and go back to previous version.")
+As of 2021/08/30, `editorconfig-mode' uses totally new implementation by
+default.  This flag disables this and go back to previous version.")
 
 ;;;###autoload
 (define-minor-mode editorconfig-mode
