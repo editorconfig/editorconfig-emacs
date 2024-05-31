857a7cea44c71ffbef999de91903891d86ce2de5
10sr <8.slashes@gmail.com>
2024-05-31 20:46:46 +0900

    Remove comment

--- ert-tests/local_variables/file_locals.rb
+++ ert-tests/local_variables/file_locals.rb
@@ -2,11 +2,6 @@
 def f
    return 1
 end
-# (hack-local-variables--find-variables)
-# file-local-variables-alist
-# (assq 'tab-width file-local-variables-alist)
-# tab-width
-
 # Local Variables:
 # ruby-indent-level: 3
 # End:
