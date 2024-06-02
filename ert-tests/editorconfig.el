2eb8a9720f6425bfb98f4b64bdca7f9d7150558a
10sr <8.slashes@gmail.com>
2024-06-02 17:39:48 +0900

    Add coding-system test

--- ert-tests/editorconfig.el
+++ ert-tests/editorconfig.el
@@ -110,6 +110,16 @@
                        write-file-functions))))
   (editorconfig-mode -1))
 
+(ert-deftest test-charset nil
+  (editorconfig-mode 1)
+  (with-visit-file (concat editorconfig-ert-dir "latin1.txt")
+    (should (eq buffer-file-coding-system
+                'iso-latin-1-unix)))
+  (with-visit-file (concat editorconfig-ert-dir "utf-16be.txt")
+    (should (eq buffer-file-coding-system
+                'utf-16be-with-signature)))
+  (editorconfig-mode -1))
+
 (ert-deftest test-local-variables nil
   (editorconfig-mode 1)
   (with-visit-file (concat editorconfig-local-variables-ert-dir "file_locals.rb")
