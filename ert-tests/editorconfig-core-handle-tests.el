906b78e04220705f076e7de5db58c31a394d02f3
10sr <8.slashes@gmail.com>
2024-07-19 01:13:38 +0900

    Scratch/editorconfig (#346)
    * Rename test files to `<foo>-tests.el`
    
    This follows the convention used in Emacs's own test suite and works around
    annoying circular `require`s when `ert-tests` happens to be in `load-path`.
    Also adjust the Makefile not to get confused by generated files.
    
    * Makefile (SRCS): Filter out ELPA-generated files.
    
    * (editorconfig--should-set): Eliminate `lisp-indent-offset` special case
    
    Instead of treating `lisp-indent-offset` specially in
    `editorconfig--should-set` (to obey `editorconfig-lisp-use-default-indent`),
    use a new function `editorconfig-set-indentation-lisp-mode`
    for Lisp modes.
    
    * lisp/editorconfig.el (editorconfig--should-set): Remove `size` argument.
    (editorconfig-set-indentation): Adjust calls accordingly.
    (editorconfig-set-indentation-lisp-mode): New function.
    (editorconfig-indentation-alist): Use it.
    
    * Fix minor cosmetic issues
    
    Mostly fix compiler warnings and prefer #' to quote function names.
    
    * editorconfig.el: Remove dependency on `nadvice` since we require
    Emacs-26 which includes `nadvice` already.
    Remove redundant `require`s.  Remove redundant `:group` arguments.
    Move `defvar`s outside of `eval-when-compile`.
    Remove "post-end trailer".
    * .dir-locals.el: New file, to replace the "post-end trailer".
    
    * ert-tests/editorconfig-core-handle-tests.el (editorconfig--fixtures):
    Rename from `fixtures`.  Change all uses.
    
    * ert-tests/editorconfig-tests.el (test-editorconfig)
    (test-local-variables, test-hack-properties-functions):
    Use `bound-and-true-p` when it's not guaranteed that the var will
    be defined.
    
    * Don't hook into `read-only-mode-hook`
    
    Don't re-set variables just because `read-only-mode` is (de)activated,
    and with Emacs-30's hooks it would be even less natural to do.
    Also `buffer-read-only` can change without calling `read-only-mode`,
    so better test it dynamically when we try to trim whitespace.
    
    * lisp/editorconfig.el (editorconfig--delete-trailing-whitespace):
    New function.
    (editorconfig-set-trailing-ws): Use it.  Use `pcase`.
    Also prefer `before-save-hook` and use `add/remove-hook` to
    manipulate hooks, like god intended.
    Don't test `buffer-read-only` any more.
    (editorconfig-mode): Don't hook into `read-only-mode-hook` any more.
    
    * ert-tests/editorconfig-tests.el (test-trim-trailing-ws): Adjust the
    test accordingly.
    
    * editorconfig-core-handle.el: Don't use file names as glob pattern
    
    Match relative file names against the glob pattern instead of
    trying to construct a glob pattern that matches the absolute
    file name (where the code failed to escape the possible
    special chars).
    
    * lisp/editorconfig-core-handle.el
    (editorconfig-core-handle-section-get-properties): Delete `dir` arg.
    (editorconfig-core-handle-get-properties-hash)
    (editorconfig-core-handle-get-properties): Adjust call accordingly.
    Use `declare` to mark it obsolete.
    (editorconfig-core-handle--fnmatch-p): Delete `dir` arg.
    
    ---------
    
    Co-authored-by: Stefan Monnier <monnier@iro.umontreal.ca>

--- ert-tests/editorconfig-core-handle-tests.el
+++ ert-tests/editorconfig-core-handle-tests.el
@@ -25,47 +25,47 @@
 
 (require 'editorconfig-core-handle)
 
-(defconst fixtures (concat (file-name-directory load-file-name) "fixtures/")
+(defconst editorconfig--fixtures (concat (file-name-directory load-file-name) "fixtures/")
   "Path to fixtures.")
 
 (set-variable 'vc-handled-backends nil)
 
 (ert-deftest test-editorconfig-core-handle ()
   ;; handle.ini
-  (let* ((conf (concat fixtures "handle.ini"))
+  (let* ((conf (concat editorconfig--fixtures "handle.ini"))
          (handle (editorconfig-core-handle conf)))
     (should (editorconfig-core-handle-root-p handle))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "b.js"))
                    '((("key2" . "value2")))))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "a.js"))
                    '((("key1" . "value1")) (("key2" . "value2"))))))
   ;; Test twice for checking cache
-  (let* ((conf (concat fixtures "handle.ini"))
+  (let* ((conf (concat editorconfig--fixtures "handle.ini"))
          (handle (editorconfig-core-handle conf)))
     (should (editorconfig-core-handle-root-p handle))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "b.js"))
                    '((("key2" . "value2")))))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "a.js"))
                    '((("key1" . "value1")) (("key2" . "value2"))))))
 
   ;; handle2.ini
-  (let* ((conf (concat fixtures "handle2.ini"))
+  (let* ((conf (concat editorconfig--fixtures "handle2.ini"))
          (handle (editorconfig-core-handle conf)))
     (should-not (editorconfig-core-handle-root-p handle))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "b.js"))
                    nil))
     (should (equal (editorconfig-core-handle-get-properties handle
-                                                            (concat fixtures
+                                                            (concat editorconfig--fixtures
                                                                     "a.js"))
                    '((("key" . "value"))))))
 
